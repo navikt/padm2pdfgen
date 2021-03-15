22e7b7ad75b9a0aa37e51ff7f2b65f9c12d396c5
John Martin Lindseth <john.martin.lindseth@nav.no>
2021-03-15 09:18:04 +0100

    Add image version to bash script

--- run_development.sh
+++ run_development.sh
@@ -2,7 +2,7 @@
 
 CURRENT_PATH="$(cd "$(dirname "$1")"; pwd)/$(basename "$1")"
 
-docker pull navikt/pdfgen
+docker pull navikt/pdfgen:096d823096fb366a6a90dfeae4401d165dd7bbb6
 docker run \
         -v $CURRENT_PATH/templates:/app/templates \
         -v $CURRENT_PATH/fonts:/app/fonts \
