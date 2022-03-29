4af181464f78bf215bb9de53801994aa84231ad5
geir-waagboe <geir.arne.waagbo@nav.no>
2022-03-29 11:01:42 +0200

    Bump pdfgen

--- run_development.sh
+++ run_development.sh
@@ -2,7 +2,7 @@
 
 CURRENT_PATH="$(cd "$(dirname "$1")"; pwd)/$(basename "$1")"
 
-docker pull navikt/pdfgen:096d823096fb366a6a90dfeae4401d165dd7bbb6
+docker pull ghcr.io/navikt/pdfgen:1.4.6
 docker run \
         -v $CURRENT_PATH/templates:/app/templates \
         -v $CURRENT_PATH/fonts:/app/fonts \
@@ -10,6 +10,7 @@ docker run \
         -v $CURRENT_PATH/resources:/app/resources \
         -p 8080:8080 \
         -e DISABLE_PDF_GET=false \
+        -e JDK_JAVA_OPTIONS \
         -it \
         --rm \
-        navikt/pdfgen
+        ghcr.io/navikt/pdfgen:1.4.6
